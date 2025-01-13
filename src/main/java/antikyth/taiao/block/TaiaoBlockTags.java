209b48fa5e5a4dc1a4a469e23ef32312ec109e86
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-01-13 14:45:37 +1300

    Add MPL-2.0 license

--- src/main/java/antikyth/taiao/block/TaiaoBlockTags.java
+++ src/main/java/antikyth/taiao/block/TaiaoBlockTags.java
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 package antikyth.taiao.block;
 
 import antikyth.taiao.Taiao;
@@ -9,6 +13,8 @@ public final class TaiaoBlockTags {
     public static final TagKey<Block> THIN_LOGS = Taiao.createTagKey("thin_logs", RegistryKeys.BLOCK);
     public static final TagKey<Block> CABBAGE_TREE_LOGS = Taiao.createTagKey("cabbage_tree_logs", RegistryKeys.BLOCK);
 
-    public static final TagKey<Block> THIN_LOG_CONNECTION_OVERRIDE = Taiao.createTagKey("thin_log_connection_override",
-            RegistryKeys.BLOCK);
+    public static final TagKey<Block> THIN_LOG_CONNECTION_OVERRIDE = Taiao.createTagKey(
+            "thin_log_connection_override",
+            RegistryKeys.BLOCK
+    );
 }
