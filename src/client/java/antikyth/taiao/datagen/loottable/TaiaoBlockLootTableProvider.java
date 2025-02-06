fc9869d37c49779987130ae86f3d255a563ac49e
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-02-06 16:55:11 +1300

    Make giant cane rushes drop seeds

--- src/client/java/antikyth/taiao/datagen/loottable/TaiaoBlockLootTableProvider.java
+++ src/client/java/antikyth/taiao/datagen/loottable/TaiaoBlockLootTableProvider.java
@@ -6,18 +6,28 @@ package antikyth.taiao.datagen.loottable;
 
 import antikyth.taiao.block.TaiaoBlocks;
 import antikyth.taiao.block.leaves.FruitLeavesBlock;
+import antikyth.taiao.block.plant.TripleTallPlantBlock;
+import antikyth.taiao.block.plant.TripleTallPlantBlock.TripleBlockPart;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.block.Block;
 import net.minecraft.data.family.BlockFamily;
 import net.minecraft.data.server.loottable.BlockLootTableGenerator;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
+import net.minecraft.loot.condition.LocationCheckLootCondition;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.predicate.BlockPredicate;
 import net.minecraft.predicate.StatePredicate;
+import net.minecraft.predicate.entity.LocationPredicate;
+import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
 
 public class TaiaoBlockLootTableProvider extends FabricBlockLootTableProvider {
@@ -28,97 +38,100 @@ public class TaiaoBlockLootTableProvider extends FabricBlockLootTableProvider {
 	@Override
 	public void generate() {
 		// Kauri foliage
-		addDrop(TaiaoBlocks.KAURI_SAPLING);
+		this.addDrop(TaiaoBlocks.KAURI_SAPLING);
 		addPottedPlantDrops(TaiaoBlocks.POTTED_KAURI_SAPLING);
-		addDrop(TaiaoBlocks.KAURI_LEAVES, block -> leavesDrops(block, TaiaoBlocks.KAURI_SAPLING, SAPLING_DROP_CHANCE));
+		this.addDrop(
+			TaiaoBlocks.KAURI_LEAVES,
+			block -> leavesDrops(block, TaiaoBlocks.KAURI_SAPLING, SAPLING_DROP_CHANCE)
+		);
 		// Kauri wood
-		addDrop(TaiaoBlocks.KAURI_LOG);
-		addDrop(TaiaoBlocks.STRIPPED_KAURI_LOG);
-		addDrop(TaiaoBlocks.KAURI_WOOD);
-		addDrop(TaiaoBlocks.STRIPPED_KAURI_WOOD);
+		this.addDrop(TaiaoBlocks.KAURI_LOG);
+		this.addDrop(TaiaoBlocks.STRIPPED_KAURI_LOG);
+		this.addDrop(TaiaoBlocks.KAURI_WOOD);
+		this.addDrop(TaiaoBlocks.STRIPPED_KAURI_WOOD);
 		// Kauri wood family
-		addDropsForFamily(TaiaoBlocks.WoodFamily.KAURI.getBlockFamily());
+		this.addDropsForFamily(TaiaoBlocks.WoodFamily.KAURI.getBlockFamily());
 
 		// Kahikatea foliage
-		addDrop(TaiaoBlocks.KAHIKATEA_SAPLING);
+		this.addDrop(TaiaoBlocks.KAHIKATEA_SAPLING);
 		addPottedPlantDrops(TaiaoBlocks.POTTED_KAHIKATEA_SAPLING);
-		addDrop(
+		this.addDrop(
 			TaiaoBlocks.KAHIKATEA_LEAVES,
 			block -> fruitLeavesDrops((FruitLeavesBlock) block, TaiaoBlocks.KAHIKATEA_SAPLING, SAPLING_DROP_CHANCE)
 		);
 		// Kahikatea wood
-		addDrop(TaiaoBlocks.KAHIKATEA_LOG);
-		addDrop(TaiaoBlocks.STRIPPED_KAHIKATEA_LOG);
-		addDrop(TaiaoBlocks.KAHIKATEA_WOOD);
-		addDrop(TaiaoBlocks.STRIPPED_KAHIKATEA_WOOD);
+		this.addDrop(TaiaoBlocks.KAHIKATEA_LOG);
+		this.addDrop(TaiaoBlocks.STRIPPED_KAHIKATEA_LOG);
+		this.addDrop(TaiaoBlocks.KAHIKATEA_WOOD);
+		this.addDrop(TaiaoBlocks.STRIPPED_KAHIKATEA_WOOD);
 		// Kahikatea wood family
-		addDropsForFamily(TaiaoBlocks.WoodFamily.KAHIKATEA.getBlockFamily());
+		this.addDropsForFamily(TaiaoBlocks.WoodFamily.KAHIKATEA.getBlockFamily());
 
 		// Rimu foliage
-		addDrop(TaiaoBlocks.RIMU_SAPLING);
+		this.addDrop(TaiaoBlocks.RIMU_SAPLING);
 		addPottedPlantDrops(TaiaoBlocks.POTTED_RIMU_SAPLING);
-		addDrop(
+		this.addDrop(
 			TaiaoBlocks.RIMU_LEAVES,
 			block -> fruitLeavesDrops((FruitLeavesBlock) block, TaiaoBlocks.RIMU_SAPLING, SAPLING_DROP_CHANCE)
 		);
 		// Rimu wood
-		addDrop(TaiaoBlocks.RIMU_LOG);
-		addDrop(TaiaoBlocks.STRIPPED_RIMU_LOG);
-		addDrop(TaiaoBlocks.RIMU_WOOD);
-		addDrop(TaiaoBlocks.STRIPPED_RIMU_WOOD);
+		this.addDrop(TaiaoBlocks.RIMU_LOG);
+		this.addDrop(TaiaoBlocks.STRIPPED_RIMU_LOG);
+		this.addDrop(TaiaoBlocks.RIMU_WOOD);
+		this.addDrop(TaiaoBlocks.STRIPPED_RIMU_WOOD);
 		// Rimu wood family
-		addDropsForFamily(TaiaoBlocks.WoodFamily.RIMU.getBlockFamily());
+		this.addDropsForFamily(TaiaoBlocks.WoodFamily.RIMU.getBlockFamily());
 
 		// Tī kōuka foliage
-		addDrop(TaiaoBlocks.CABBAGE_TREE_SAPLING);
+		this.addDrop(TaiaoBlocks.CABBAGE_TREE_SAPLING);
 		addPottedPlantDrops(TaiaoBlocks.POTTED_CABBAGE_TREE_SAPLING);
-		addDrop(
+		this.addDrop(
 			TaiaoBlocks.CABBAGE_TREE_LEAVES,
 			block -> leavesDrops(block, TaiaoBlocks.CABBAGE_TREE_SAPLING, SAPLING_DROP_CHANCE)
 		);
 		// Tī kōuka wood
-		addDrop(TaiaoBlocks.CABBAGE_TREE_LOG);
-		addDrop(TaiaoBlocks.CABBAGE_TREE_WOOD);
-		addDrop(TaiaoBlocks.STRIPPED_CABBAGE_TREE_LOG);
-		addDrop(TaiaoBlocks.STRIPPED_CABBAGE_TREE_WOOD);
+		this.addDrop(TaiaoBlocks.CABBAGE_TREE_LOG);
+		this.addDrop(TaiaoBlocks.CABBAGE_TREE_WOOD);
+		this.addDrop(TaiaoBlocks.STRIPPED_CABBAGE_TREE_LOG);
+		this.addDrop(TaiaoBlocks.STRIPPED_CABBAGE_TREE_WOOD);
 
 		// Mamaku foliage
-		addDrop(TaiaoBlocks.MAMAKU_SAPLING);
+		this.addDrop(TaiaoBlocks.MAMAKU_SAPLING);
 		addPottedPlantDrops(TaiaoBlocks.POTTED_MAMAKU_SAPLING);
-		addDrop(
+		this.addDrop(
 			TaiaoBlocks.MAMAKU_LEAVES,
 			block -> leavesDrops(block, TaiaoBlocks.MAMAKU_SAPLING, SAPLING_DROP_CHANCE)
 		);
 		// Mamaku wood
-		addDrop(TaiaoBlocks.MAMAKU_LOG);
-		addDrop(TaiaoBlocks.MAMAKU_WOOD);
-		addDrop(TaiaoBlocks.STRIPPED_MAMAKU_LOG);
-		addDrop(TaiaoBlocks.STRIPPED_MAMAKU_WOOD);
+		this.addDrop(TaiaoBlocks.MAMAKU_LOG);
+		this.addDrop(TaiaoBlocks.MAMAKU_WOOD);
+		this.addDrop(TaiaoBlocks.STRIPPED_MAMAKU_LOG);
+		this.addDrop(TaiaoBlocks.STRIPPED_MAMAKU_WOOD);
 		// Mamaku wood family
-		addDropsForFamily(TaiaoBlocks.WoodFamily.MAMAKU.getBlockFamily());
+		this.addDropsForFamily(TaiaoBlocks.WoodFamily.MAMAKU.getBlockFamily());
 
 		// Whekī ponga foliage
-		addDrop(TaiaoBlocks.WHEKII_PONGA_SAPLING);
+		this.addDrop(TaiaoBlocks.WHEKII_PONGA_SAPLING);
 		addPottedPlantDrops(TaiaoBlocks.POTTED_WHEKII_PONGA_SAPLING);
-		addDrop(
+		this.addDrop(
 			TaiaoBlocks.WHEKII_PONGA_LEAVES,
 			block -> leavesDrops(block, TaiaoBlocks.WHEKII_PONGA_SAPLING, SAPLING_DROP_CHANCE)
 		);
 		// Whekī ponga wood
-		addDrop(TaiaoBlocks.WHEKII_PONGA_LOG);
-		addDrop(TaiaoBlocks.WHEKII_PONGA_WOOD);
-		addDrop(TaiaoBlocks.STRIPPED_WHEKII_PONGA_LOG);
-		addDrop(TaiaoBlocks.STRIPPED_WHEKII_PONGA_WOOD);
+		this.addDrop(TaiaoBlocks.WHEKII_PONGA_LOG);
+		this.addDrop(TaiaoBlocks.WHEKII_PONGA_WOOD);
+		this.addDrop(TaiaoBlocks.STRIPPED_WHEKII_PONGA_LOG);
+		this.addDrop(TaiaoBlocks.STRIPPED_WHEKII_PONGA_WOOD);
 
-		addDrop(TaiaoBlocks.RAUPOO, BlockLootTableGenerator::dropsWithShears);
-		addDrop(TaiaoBlocks.GIANT_CANE_RUSH, BlockLootTableGenerator::dropsWithShears);
+		this.addDrop(TaiaoBlocks.GIANT_CANE_RUSH, block -> tripleTallPlantDrops(block, Items.WHEAT_SEEDS, 0.125f));
+		this.addDrop(TaiaoBlocks.RAUPOO, BlockLootTableGenerator::dropsWithShears);
 	}
 
 	public void addDropsForFamily(@NotNull BlockFamily family) {
-		addDrop(family.getBaseBlock());
+		this.addDrop(family.getBaseBlock());
 
 		for (Block block : family.getVariants().values()) {
-			addDrop(block);
+			this.addDrop(block);
 		}
 	}
 
@@ -136,5 +149,57 @@ public class TaiaoBlockLootTableProvider extends FabricBlockLootTableProvider {
 					.apply(ApplyBonusLootFunction.uniformBonusCount(Enchantments.FORTUNE))
 					.with(ItemEntry.builder(leaves.fruitItem))
 			);
+	}
+
+	public LootTable.Builder tripleTallPlantDrops(Block tripleTallPlant, ItemConvertible seeds, float seedsChance) {
+		LootTable.Builder table = LootTable.builder();
+
+		LootPoolEntry.Builder<?> drops = ItemEntry.builder(tripleTallPlant)
+			.conditionally(WITH_SHEARS)
+			.alternatively(
+				this.addSurvivesExplosionCondition(tripleTallPlant, ItemEntry.builder(seeds))
+					.conditionally(RandomChanceLootCondition.builder(seedsChance))
+			);
+
+		// For each block part, validate that the others are in the correct positions.
+		for (int i = 0; i < TripleBlockPart.values().length; i++) {
+			TripleBlockPart part = TripleBlockPart.values()[i];
+
+			LootPool.Builder pool = LootPool.builder()
+				.with(drops)
+				.conditionally(
+					BlockStatePropertyLootCondition.builder(tripleTallPlant)
+						.properties(StatePredicate.Builder.create().exactMatch(TripleTallPlantBlock.PART, part))
+				);
+
+			// Ensure that the other parts are in the correct positions.
+			for (int j = 0; j < TripleBlockPart.values().length; j++) {
+				if (j == i) continue;
+
+				TripleBlockPart otherPart = TripleBlockPart.values()[j];
+				BlockPos relativePos = new BlockPos(0, j - i, 0);
+
+				pool.conditionally(
+					LocationCheckLootCondition.builder(
+						LocationPredicate.Builder.create()
+							.block(
+								BlockPredicate.Builder.create()
+									.blocks(tripleTallPlant)
+									.state(
+										StatePredicate.Builder.create()
+											.exactMatch(TripleTallPlantBlock.PART, otherPart)
+											.build()
+									)
+									.build()
+							),
+						relativePos
+					)
+				);
+			}
+
+			table.pool(pool);
+		}
+
+		return table;
 	}
 }
