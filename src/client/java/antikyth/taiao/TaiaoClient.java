081c41b16da713e6b50d0d98ffe297a7e65df55c
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-02-06 15:12:16 +1300

    Add giant cane rush textures

--- src/client/java/antikyth/taiao/TaiaoClient.java
+++ src/client/java/antikyth/taiao/TaiaoClient.java
@@ -26,113 +26,114 @@ import net.minecraft.world.BlockRenderView;
 import org.jetbrains.annotations.NotNull;
 
 public class TaiaoClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-        TaiaoEntityModels.initialize();
+	@Override
+	public void onInitializeClient() {
+		TaiaoEntityModels.initialize();
 
-        registerColors();
-        registerRenderLayers();
-    }
+		registerColors();
+		registerRenderLayers();
+	}
 
-    public static void registerColors() {
-        Taiao.LOGGER.debug("Registering block and item dynamic color providers");
+	public static void registerColors() {
+		Taiao.LOGGER.debug("Registering block and item dynamic color providers");
 
-        // Leaves
-        registerFoliageColors(
-                TaiaoBlocks.KAURI_LEAVES,
-                TaiaoBlocks.KAHIKATEA_LEAVES,
-                TaiaoBlocks.RIMU_LEAVES,
-                TaiaoBlocks.CABBAGE_TREE_LEAVES,
-                TaiaoBlocks.MAMAKU_LEAVES,
-                TaiaoBlocks.WHEKII_PONGA_LEAVES
-        );
+		// Leaves
+		registerFoliageColors(
+			TaiaoBlocks.KAURI_LEAVES,
+			TaiaoBlocks.KAHIKATEA_LEAVES,
+			TaiaoBlocks.RIMU_LEAVES,
+			TaiaoBlocks.CABBAGE_TREE_LEAVES,
+			TaiaoBlocks.MAMAKU_LEAVES,
+			TaiaoBlocks.WHEKII_PONGA_LEAVES
+		);
 
-        // Spawn eggs
-        registerSpawnEggColors(
-                TaiaoItems.KIWI_SPAWN_EGG,
-                TaiaoItems.PUUKEKO_SPAWN_EGG,
-                TaiaoItems.MOA_SPAWN_EGG,
-                TaiaoItems.KAAKAAPOO_SPAWN_EGG
-        );
-    }
+		// Spawn eggs
+		registerSpawnEggColors(
+			TaiaoItems.KIWI_SPAWN_EGG,
+			TaiaoItems.PUUKEKO_SPAWN_EGG,
+			TaiaoItems.MOA_SPAWN_EGG,
+			TaiaoItems.KAAKAAPOO_SPAWN_EGG
+		);
+	}
 
-    public static void registerRenderLayers() {
-        Taiao.LOGGER.debug("Registering block render layers");
+	public static void registerRenderLayers() {
+		Taiao.LOGGER.debug("Registering block render layers");
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.KAURI_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_KAURI_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.KAURI_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_KAURI_SAPLING, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.KAHIKATEA_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_KAHIKATEA_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.KAHIKATEA_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_KAHIKATEA_SAPLING, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.RIMU_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_RIMU_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.RIMU_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_RIMU_SAPLING, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.CABBAGE_TREE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_CABBAGE_TREE_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.CABBAGE_TREE_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_CABBAGE_TREE_SAPLING, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.MAMAKU_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_MAMAKU_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.MAMAKU_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_MAMAKU_SAPLING, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.WHEKII_PONGA_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_WHEKII_PONGA_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.WHEKII_PONGA_SAPLING, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.POTTED_WHEKII_PONGA_SAPLING, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.RAUPOO, RenderLayer.getCutout());
-    }
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.RAUPOO, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TaiaoBlocks.GIANT_CANE_RUSH, RenderLayer.getCutout());
+	}
 
-    public static void registerSpawnEggColors(Item @NotNull ... items) {
-        registerItemColors(TaiaoClient::spawnEggColor, items);
-    }
+	public static void registerSpawnEggColors(Item @NotNull ... items) {
+		registerItemColors(TaiaoClient::spawnEggColor, items);
+	}
 
-    public static void registerItemColors(ItemColorProvider colorProvider, Item @NotNull ... items) {
-        for (Item item : items) {
-            ColorProviderRegistry.ITEM.register(colorProvider, item);
-        }
-    }
+	public static void registerItemColors(ItemColorProvider colorProvider, Item @NotNull ... items) {
+		for (Item item : items) {
+			ColorProviderRegistry.ITEM.register(colorProvider, item);
+		}
+	}
 
-    public static void registerFoliageColors(Block @NotNull ... blocks) {
-        registerBlockColors(TaiaoClient::foliageBlockColor, TaiaoClient::foliageItemColor, blocks);
-    }
+	public static void registerFoliageColors(Block @NotNull ... blocks) {
+		registerBlockColors(TaiaoClient::foliageBlockColor, TaiaoClient::foliageItemColor, blocks);
+	}
 
-    public static void registerBlockColors(BlockColorProvider colorProvider, Block @NotNull ... blocks) {
-        for (Block block : blocks) {
-            ColorProviderRegistry.BLOCK.register(colorProvider, block);
-        }
-    }
+	public static void registerBlockColors(BlockColorProvider colorProvider, Block @NotNull ... blocks) {
+		for (Block block : blocks) {
+			ColorProviderRegistry.BLOCK.register(colorProvider, block);
+		}
+	}
 
-    public static void registerBlockColors(
-            BlockColorProvider blockColorProvider,
-            ItemColorProvider itemColorProvider,
-            Block @NotNull ... blocks
-    ) {
-        for (Block block : blocks) {
-            ColorProviderRegistry.BLOCK.register(blockColorProvider, block);
-            ColorProviderRegistry.ITEM.register(itemColorProvider, block.asItem());
-        }
-    }
+	public static void registerBlockColors(
+		BlockColorProvider blockColorProvider,
+		ItemColorProvider itemColorProvider,
+		Block @NotNull ... blocks
+	) {
+		for (Block block : blocks) {
+			ColorProviderRegistry.BLOCK.register(blockColorProvider, block);
+			ColorProviderRegistry.ITEM.register(itemColorProvider, block.asItem());
+		}
+	}
 
-    public static int foliageBlockColor(BlockState state, BlockRenderView world, BlockPos pos, int tintIndex) {
-        return (world != null && pos != null) ? BiomeColors.getFoliageColor(world, pos)
-                : FoliageColors.getDefaultColor();
-    }
+	public static int foliageBlockColor(BlockState state, BlockRenderView world, BlockPos pos, int tintIndex) {
+		return (world != null && pos != null) ? BiomeColors.getFoliageColor(world, pos)
+			: FoliageColors.getDefaultColor();
+	}
 
-    public static int grassBlockColor(BlockState state, BlockRenderView world, BlockPos pos, int tintIndex) {
-        return (world != null && pos != null) ? BiomeColors.getGrassColor(world, pos) : GrassColors.getDefaultColor();
-    }
+	public static int grassBlockColor(BlockState state, BlockRenderView world, BlockPos pos, int tintIndex) {
+		return (world != null && pos != null) ? BiomeColors.getGrassColor(world, pos) : GrassColors.getDefaultColor();
+	}
 
-    public static int foliageItemColor(ItemStack stack, int tintIndex) {
-        return FoliageColors.getDefaultColor();
-    }
+	public static int foliageItemColor(ItemStack stack, int tintIndex) {
+		return FoliageColors.getDefaultColor();
+	}
 
-    public static int grassItemColor(ItemStack stack, int tintIndex) {
-        return GrassColors.getDefaultColor();
-    }
+	public static int grassItemColor(ItemStack stack, int tintIndex) {
+		return GrassColors.getDefaultColor();
+	}
 
-    public static int spawnEggColor(@NotNull ItemStack stack, int tintIndex) {
-        if (stack.getItem() instanceof SpawnEggItem item) {
-            return item.getColor(tintIndex);
-        } else {
-            return 0;
-        }
-    }
+	public static int spawnEggColor(@NotNull ItemStack stack, int tintIndex) {
+		if (stack.getItem() instanceof SpawnEggItem item) {
+			return item.getColor(tintIndex);
+		} else {
+			return 0;
+		}
+	}
 }
