c7ea3c7985d9da8bdcf1fd269bba01b0c0d09e84
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-02-07 12:53:29 +1300

    Make some slight code tweaks

--- src/client/java/antikyth/taiao/entity/render/model/KiwiEntityModel.java
+++ src/client/java/antikyth/taiao/entity/render/model/KiwiEntityModel.java
@@ -14,75 +14,73 @@ import org.jetbrains.annotations.NotNull;
 
 // Made with Blockbench 4.12.1
 public class KiwiEntityModel<E extends Entity> extends AnimalModel<E> {
-    /**
-     * The default pitch of the kiwi's head in degrees.
-     */
-    private static final float HEAD_PITCH = 30f;
+	/**
+	 * The default pitch of the kiwi's head in degrees.
+	 */
+	private static final float HEAD_PITCH = 30f;
 
-    private final ModelPart body;
-    private final ModelPart leftLeg;
-    private final ModelPart rightLeg;
-    private final ModelPart head;
+	private final ModelPart body;
+	private final ModelPart leftLeg;
+	private final ModelPart rightLeg;
+	private final ModelPart head;
 
-    public KiwiEntityModel(@NotNull ModelPart root) {
-        super(true, 9.5f, 1f);
+	public KiwiEntityModel(@NotNull ModelPart root) {
+		super(true, 9.5f, 1f);
 
-        this.body = root.getChild("body");
-        this.leftLeg = root.getChild("left_leg");
-        this.rightLeg = root.getChild("right_leg");
-        this.head = root.getChild("head");
-    }
+		this.body = root.getChild("body");
+		this.leftLeg = root.getChild("left_leg");
+		this.rightLeg = root.getChild("right_leg");
+		this.head = root.getChild("head");
+	}
 
-    public static @NotNull TexturedModelData getTexturedModelData() {
-        ModelData modelData = new ModelData();
-        ModelPartData modelPartData = modelData.getRoot();
+	public static @NotNull TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
 
-        modelPartData.addChild(
-                "body",
-                ModelPartBuilder.create().uv(0, 0).cuboid(-5f, -8f, -3f, 6f, 6f, 7f),
-                ModelTransform.pivot(2f, 24f, 0f)
-        );
+		modelPartData.addChild(
+			"body",
+			ModelPartBuilder.create().uv(0, 0).cuboid(-5f, -8f, -3f, 6f, 6f, 7f),
+			ModelTransform.pivot(2f, 24f, 0f)
+		);
 
-        ModelPartBuilder leg = ModelPartBuilder.create().uv(0, 19).cuboid(-1.5f, 0f, -1f, 3f, 2f, 2f);
+		ModelPartBuilder leg = ModelPartBuilder.create().uv(0, 19).cuboid(-1.5f, 0f, -1f, 3f, 2f, 2f);
 
-        modelPartData.addChild("left_leg", leg, ModelTransform.pivot(1.5f, 22f, 0f));
-        modelPartData.addChild("right_leg", leg, ModelTransform.pivot(-1.5f, 22f, 0f));
+		modelPartData.addChild("left_leg", leg, ModelTransform.pivot(1.5f, 22f, 0f));
+		modelPartData.addChild("right_leg", leg, ModelTransform.pivot(-1.5f, 22f, 0f));
 
-        modelPartData.addChild(
-                "head",
-                ModelPartBuilder.create().uv(0, 13).cuboid(-1.5f, -1.5f, -3f, 3f, 3f, 3f)
-                        .uv(12, 13).cuboid(-0.5f, -0.5f, -8f, 1f, 1f, 5f),
-                ModelTransform.pivot(0f, 18f, -3f)
-        );
+		modelPartData.addChild(
+			"head",
+			ModelPartBuilder.create().uv(0, 13).cuboid(-1.5f, -1.5f, -3f, 3f, 3f, 3f)
+				.uv(12, 13).cuboid(-0.5f, -0.5f, -8f, 1f, 1f, 5f),
+			ModelTransform.pivot(0f, 18f, -3f)
+		);
 
-        return TexturedModelData.of(modelData, 32, 32);
-    }
+		return TexturedModelData.of(modelData, 32, 32);
+	}
 
-    @Override
-    public void setAngles(
-            E entity,
-            float limbSwing,
-            float limbSwingAmount,
-            float ageInTicks,
-            float headYawDegrees,
-            float headPitchDegrees
-    ) {
-        headPitchDegrees += HEAD_PITCH;
+	@Override
+	public void setAngles(
+		E entity,
+		float limbSwing,
+		float limbSwingAmount,
+		float ageInTicks,
+		float headYawDegrees,
+		float headPitchDegrees
+	) {
+		this.head.pitch = Taiao.degreesToRadians(headPitchDegrees + HEAD_PITCH);
+		this.head.yaw = Taiao.degreesToRadians(headYawDegrees);
 
-        this.head.pitch = Taiao.degreesToRadians(headPitchDegrees);
-        this.head.yaw = Taiao.degreesToRadians(headYawDegrees);
+		this.rightLeg.pitch = MathHelper.cos(limbSwing * 0.6662f) * 1.4f * limbSwingAmount;
+		this.leftLeg.pitch = MathHelper.cos(limbSwing * 0.6662f + (float) Math.PI) * 1.4f * limbSwingAmount;
+	}
 
-        this.rightLeg.pitch = MathHelper.cos(limbSwing * 0.6662f) * 1.4f * limbSwingAmount;
-        this.leftLeg.pitch = MathHelper.cos(limbSwing * 0.6662f + (float) Math.PI) * 1.4f * limbSwingAmount;
-    }
+	@Override
+	protected Iterable<ModelPart> getHeadParts() {
+		return ImmutableList.of(this.head);
+	}
 
-    @Override
-    protected Iterable<ModelPart> getHeadParts() {
-        return ImmutableList.of(this.head);
-    }
-
-    @Override
-    protected Iterable<ModelPart> getBodyParts() {
-        return ImmutableList.of(this.body, this.leftLeg, this.rightLeg);
-    }
+	@Override
+	protected Iterable<ModelPart> getBodyParts() {
+		return ImmutableList.of(this.body, this.leftLeg, this.rightLeg);
+	}
 }
--- src/main/java/antikyth/taiao/mixin/AxeItemMixin.java
+++ src/main/java/antikyth/taiao/mixin/AxeItemMixin.java
@@ -9,6 +9,7 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.AxeItem;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -17,18 +18,18 @@ import java.util.Optional;
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 @Mixin(AxeItem.class)
 public class AxeItemMixin {
-    /**
-     * Adds support for {@link Strippable} blocks that would otherwise not be supported for stripping due to their lack
-     * of an {@link net.minecraft.state.property.Properties#AXIS AXIS} property.
-     */
-    @ModifyReturnValue(
-            method = "getStrippedState(Lnet/minecraft/block/BlockState;)Ljava/util/Optional;",
-            at = @At("RETURN")
-    )
-    private Optional<BlockState> addStrippedStateOptions(
-            Optional<BlockState> original,
-            @Local(argsOnly = true) BlockState state
-    ) {
-        return original.or(() -> Strippable.getStrippedState(state));
-    }
+	/**
+	 * Adds support for {@link Strippable} blocks that would otherwise not be supported for stripping due to their lack
+	 * of an {@link net.minecraft.state.property.Properties#AXIS AXIS} property.
+	 */
+	@ModifyReturnValue(
+		method = "getStrippedState(Lnet/minecraft/block/BlockState;)Ljava/util/Optional;",
+		at = @At("RETURN")
+	)
+	private @NotNull Optional<BlockState> addStrippedStateOptions(
+		@NotNull Optional<BlockState> original,
+		@Local(argsOnly = true) BlockState state
+	) {
+		return original.or(() -> Strippable.getStrippedState(state));
+	}
 }
