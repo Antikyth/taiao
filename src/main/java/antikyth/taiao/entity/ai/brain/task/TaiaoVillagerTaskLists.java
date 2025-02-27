cf48a3281d3cfd042a332d4c55da402592f7127a
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-02-27 20:23:04 +1300

    Make villagers treat kākāpō the same as cats
    Closes #128

--- src/main/java/antikyth/taiao/entity/ai/brain/task/TaiaoVillagerTaskLists.java
+++ src/main/java/antikyth/taiao/entity/ai/brain/task/TaiaoVillagerTaskLists.java
@@ -4,8 +4,12 @@
 
 package antikyth.taiao.entity.ai.brain.task;
 
+import antikyth.taiao.entity.TaiaoEntities;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.ai.brain.task.FindEntityTask;
+import net.minecraft.entity.ai.brain.task.LookAtMobTask;
 import net.minecraft.entity.ai.brain.task.Task;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.village.VillagerProfession;
@@ -19,6 +23,55 @@ public class TaiaoVillagerTaskLists {
 		float speed
 	) {
 		return ImmutableList.<Pair<? extends Task<? super VillagerEntity>, Integer>>builder()
+			// Harvest harakeke
 			.add(Pair.of(new HarvestHarakekeTask(speed), profession == VillagerProfession.SHEPHERD ? 2 : 5));
+	}
+
+	@Contract("_ -> new")
+	public static ImmutableList.@NotNull Builder<Pair<? extends Task<? super VillagerEntity>, Integer>> createRandomPlayTasks(
+		float speed
+	) {
+		return ImmutableList.<Pair<? extends Task<? super VillagerEntity>, Integer>>builder()
+			// Play with kākāpō
+			.add(
+				Pair.of(
+					FindEntityTask.create(
+						TaiaoEntities.KAAKAAPOO,
+						8,
+						MemoryModuleType.INTERACTION_TARGET,
+						speed,
+						2
+					),
+					1
+				)
+			);
+	}
+
+	@Contract("_, _ -> new")
+	public static ImmutableList.@NotNull Builder<Pair<? extends Task<? super VillagerEntity>, Integer>> createRandomIdleTasks(
+		VillagerProfession profession,
+		float speed
+	) {
+		return ImmutableList.<Pair<? extends Task<? super VillagerEntity>, Integer>>builder()
+			// Admire kākāpō
+			.add(
+				Pair.of(
+					FindEntityTask.create(
+						TaiaoEntities.KAAKAAPOO,
+						8,
+						MemoryModuleType.INTERACTION_TARGET,
+						speed,
+						2
+					),
+					1
+				)
+			);
+	}
+
+	@Contract(" -> new")
+	public static ImmutableList.@NotNull Builder<Pair<? extends Task<? super VillagerEntity>, Integer>> createRandomFreeFollowTasks() {
+		return ImmutableList.<Pair<? extends Task<? super VillagerEntity>, Integer>>builder()
+			// Follow kākāpō
+			.add(Pair.of(LookAtMobTask.create(TaiaoEntities.KAAKAAPOO, 8f), 8));
 	}
 }
--- src/main/java/antikyth/taiao/mixin/entity/villager/VillagerTaskListProviderMixin.java
+++ src/main/java/antikyth/taiao/mixin/entity/villager/VillagerTaskListProviderMixin.java
@@ -8,6 +8,8 @@ import antikyth.taiao.entity.ai.brain.task.TaiaoVillagerTaskLists;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.entity.ai.brain.MemoryModuleState;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.RandomTask;
 import net.minecraft.entity.ai.brain.task.Task;
 import net.minecraft.entity.ai.brain.task.VillagerTaskListProvider;
@@ -19,6 +21,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.List;
+import java.util.Map;
 
 @Mixin(VillagerTaskListProvider.class)
 public class VillagerTaskListProviderMixin {
@@ -36,5 +39,54 @@ public class VillagerTaskListProviderMixin {
 		float speed
 	) {
 		return constructor.call(TaiaoVillagerTaskLists.createRandomWorkTasks(profession, speed).addAll(tasks).build());
+	}
+
+	/**
+	 * Adds play tasks to villagers' existing {@link RandomTask} play task.
+	 */
+	@WrapOperation(
+		method = "createPlayTasks",
+		at = @At(value = "NEW", target = "net/minecraft/entity/ai/brain/task/RandomTask")
+	)
+	private static RandomTask<VillagerEntity> addTasksToRandomPlayTasks(
+		@Unmodifiable Map<MemoryModuleType<?>, MemoryModuleState> requiredMemoryState,
+		@Unmodifiable List<Pair<? extends Task<? super VillagerEntity>, Integer>> tasks,
+		@NotNull Operation<RandomTask<VillagerEntity>> constructor,
+		float speed
+	) {
+		return constructor.call(
+			requiredMemoryState,
+			TaiaoVillagerTaskLists.createRandomPlayTasks(speed).addAll(tasks).build()
+		);
+	}
+
+	/**
+	 * Adds idle tasks to villagers' existing {@link RandomTask} idle task.
+	 */
+	@WrapOperation(
+		method = "createIdleTasks",
+		at = @At(value = "NEW", target = "net/minecraft/entity/ai/brain/task/RandomTask")
+	)
+	private static RandomTask<VillagerEntity> addTasksToRandomIdleTasks(
+		@Unmodifiable List<Pair<? extends Task<? super VillagerEntity>, Integer>> tasks,
+		@NotNull Operation<RandomTask<VillagerEntity>> constructor,
+		VillagerProfession profession,
+		float speed
+	) {
+		return constructor.call(TaiaoVillagerTaskLists.createRandomIdleTasks(profession, speed).addAll(tasks).build());
+	}
+
+	/**
+	 * Adds free-follow tasks to villagers' existing {@link RandomTask} free-follow task.
+	 */
+	@WrapOperation(
+		method = "createFreeFollowTask",
+		at = @At(value = "NEW", target = "net/minecraft/entity/ai/brain/task/RandomTask")
+	)
+	private static RandomTask<VillagerEntity> addTasksToRandomFreeFollowTasks(
+		@Unmodifiable List<Pair<? extends Task<? super VillagerEntity>, Integer>> tasks,
+		@NotNull Operation<RandomTask<VillagerEntity>> constructor
+	) {
+		return constructor.call(TaiaoVillagerTaskLists.createRandomFreeFollowTasks().addAll(tasks).build());
 	}
 }
