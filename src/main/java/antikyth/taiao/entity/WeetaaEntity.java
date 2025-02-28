d4c0f666323b80b7f4ee1d47507fbf19367129cb
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-02-28 22:34:21 +1300

    Make wētā climb walls

--- src/main/java/antikyth/taiao/entity/WeetaaEntity.java
+++ src/main/java/antikyth/taiao/entity/WeetaaEntity.java
@@ -4,20 +4,37 @@
 
 package antikyth.taiao.entity;
 
+import antikyth.taiao.block.TaiaoBlockTags;
 import antikyth.taiao.sound.TaiaoSoundEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.NotNull;
 
 public class WeetaaEntity extends PathAwareEntity {
+	/**
+	 * Flags for the wētā.
+	 */
+	protected static final TrackedData<Byte> FLAGS = DataTracker.registerData(
+		WeetaaEntity.class,
+		TrackedDataHandlerRegistry.BYTE
+	);
+	protected static final int CLIMBING_FLAG = 0b0000_0001;
+
 	protected WeetaaEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
 		super(entityType, world);
 	}
@@ -57,5 +74,33 @@ public class WeetaaEntity extends PathAwareEntity {
 		BlockState below = world.getBlockState(pos.down());
 
 		return below.isIn(TaiaoBlockTags.WEETAA_SPAWNABLE_ON);
+	}
+
+	@Override
+	protected void initDataTracker() {
+		super.initDataTracker();
+
+		this.dataTracker.startTracking(FLAGS, (byte) 0);
+	}
+
+	@Override
+	public boolean isClimbing() {
+		return (this.dataTracker.get(FLAGS) & CLIMBING_FLAG) != 0;
+	}
+
+	public void setClimbing(boolean climbing) {
+		byte flags = this.dataTracker.get(FLAGS);
+
+		if (climbing) flags = (byte) (flags | CLIMBING_FLAG);
+		else flags = (byte) (flags & ~CLIMBING_FLAG);
+
+		this.dataTracker.set(FLAGS, flags);
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+
+		if (!this.getWorld().isClient) this.setClimbing(this.horizontalCollision);
 	}
 }
