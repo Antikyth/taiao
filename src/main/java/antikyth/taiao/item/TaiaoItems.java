9057b2bb2888cbff7a85bd54531a47d84da5ad17
Zoe <104020300+Antikyth@users.noreply.github.com>
2025-03-23 19:03:25 +1300

    Haast's eagle spawn egg colors

--- src/main/java/antikyth/taiao/item/TaiaoItems.java
+++ src/main/java/antikyth/taiao/item/TaiaoItems.java
@@ -85,7 +85,7 @@ public class TaiaoItems {
 	);
 	public static final Item HAASTS_EAGLE_SPAWN_EGG = register(
 		Taiao.id("haasts_eagle_spawn_egg"),
-		new SpawnEggItem(TaiaoEntities.HAASTS_EAGLE, 0x2c180c, 0x55361c, new FabricItemSettings())
+		new SpawnEggItem(TaiaoEntities.HAASTS_EAGLE, 0xb7aea9, 0x7b512c, new FabricItemSettings())
 	);
 	public static final Item MOA_SPAWN_EGG = register(
 		Taiao.id("moa_spawn_egg"),
